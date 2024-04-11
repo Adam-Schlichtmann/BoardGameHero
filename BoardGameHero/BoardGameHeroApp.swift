5bb0cfcd192d2751067b70bd6d1ff83a2e017c54
Adam-Schlichtmann <adam.schlichtmann1113@gmail.com>
2024-04-11 12:50:17 -0500

    Initial Commit

---
+++ BoardGameHero/BoardGameHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardGameHeroApp.swift
+//  BoardGameHero
+//
+//  Created by Adam Schlichtmann on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BoardGameHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
